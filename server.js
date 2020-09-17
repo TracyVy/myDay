98cb90882b6114bf37a50b2757a9284845934cf8
Tracy Vy <tracyvy88@gmail.com>
2020-09-16 19:40:27 -0700

    edit production

--- server.js
+++ server.js
@@ -25,10 +25,16 @@ const apiRoutes = require("./routes/api-routes");
 app.use(apiRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  const root = require("path").join(__dirname, "client", "build");
+  app.use(express.static(root));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile("index.html", { root });
   });
+
+  // app.use(express.static("client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 app.listen(PORT, () => {
